c86a7a3db4feaba661c399ecd62bd7fb86da9216
Przemek <przemek.bzowka@gmail.com>
2019-10-31 04:09:56 +0100

    initail commit

---
+++ app/js/index.js
@@ -0,0 +1,17 @@
+const openMenuBtn = document.querySelector("#open-menu-btn-js");
+const closeMenuBtn = document.querySelector("#close-menu-btn-js");
+const menuContent = document.querySelector("#mobile-menu-js");
+const menuMobileLinks = document.querySelectorAll(".mobile-menu__item");
+
+function menuOpen() {
+  menuContent.classList.add("mobile-menu--on");
+}
+
+function menuClose() {
+  menuContent.classList.remove("mobile-menu--on");
+}
+
+openMenuBtn.addEventListener("click", menuOpen);
+closeMenuBtn.addEventListener("click", menuClose);
+
+menuMobileLinks.forEach(el => el.addEventListener("click", menuClose));
